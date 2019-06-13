4a51cc4fc7e447591e287f6d5a741314ac540b6a
MrT3313 <rturgeon13@gmail.com>
2019-06-13 12:33:54 -0500

    login form created

--- webauth_3_frontend/src/auth/Login.js
+++ webauth_3_frontend/src/auth/Login.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 export default class Login extends React.Component {
+    // State //
+
+    // Methods //
+    formSubmit_handler = e => {
+        e.preventDefault();
+        console.log('you clicked the form button!')
+    }
+
     render() {
         return (
             <div>
-                HELLO FROM LOGIN
+                <form onSubmit={this.formSubmit_handler} >
+                    <input
+                        type='text'
+                        placeholder='userName'
+                    ></input>
+                    <input
+                        type='password'
+                        placeholder='password'
+                    ></input>
+                    <button>Click</button>
+                </form>
             </div>
         )
     }
