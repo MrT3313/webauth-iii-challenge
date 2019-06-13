61431210f277c4b2de2ce37f30b26939bd762f7a
MrT3313 <rturgeon13@gmail.com>
2019-06-13 16:07:43 -0500

    JWT implemented

--- webauth_3_backend/api/routers/loginRouter.js
+++ webauth_3_backend/api/routers/loginRouter.js
@@ -1,6 +1,9 @@
 // IMPORTS\
     const express = require('express')
     const bcrypt = require('bcrypt')
+    const jwt = require('jsonwebtoken')
+    const secrets = require('../../secrets')
+    
     
 // KNEX DB
     DB_KNEX = require('../../data/dbConfig.js')
@@ -24,7 +27,25 @@
                     const pwVerificatoin = bcrypt.compareSync(password, user.password)
                     if (user && pwVerificatoin) {
                         console.log('USER IS A MATCH')
-                        res.status(200).json( {message: `Welcome ${userName}`} )
+
+                        console.log('secrets', secrets)
+                        console.log('secrets.jwtSecret', secrets.jwtSecret)
+
+                        const token = jwt.sign(
+                            {
+                                userID: user.userID,
+                                userName: user.userName
+                            },
+                            secrets.jwtSecret,
+                            {
+                                expiresIn: "1d"
+                            }
+                        )
+
+                        res.status(200).json({
+                            message: `Welcome ${userName}`, 
+                            token
+                        })
                     } else {
                         res.status(401).json( { error: 'Unable to Login'} )
                     }
