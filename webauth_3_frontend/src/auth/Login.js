34e5d80c01386c99c52735bea3a54dcef07c7302
MrT3313 <rturgeon13@gmail.com>
2019-06-13 12:49:47 -0500

    inputs setting <Login /> state

--- webauth_3_frontend/src/auth/Login.js
+++ webauth_3_frontend/src/auth/Login.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 export default class Login extends React.Component {
     // State //
+    state = {
+        userName: "",
+        password: ""
+    }
 
     // Methods //
     formSubmit_handler = e => {
         e.preventDefault();
         console.log('you clicked the form button!')
+    }
+    inputChange_handler = e => {
+        e.preventDefault();
+        console.log('inside input change handler')
+
+        this.setState({ [e.target.name]: e.target.value });
     }
 
     render() {
@@ -15,11 +25,15 @@ export default class Login extends React.Component {
                 <form onSubmit={this.formSubmit_handler} >
                     <input
                         type='text'
+                        name="userName"
                         placeholder='userName'
+                        onChange={this.inputChange_handler}
                     ></input>
                     <input
                         type='password'
+                        name="password"
                         placeholder='password'
+                        onChange={this.inputChange_handler}
                     ></input>
                     <button>Click</button>
                 </form>
